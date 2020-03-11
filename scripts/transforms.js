aa34bb5c223ffdcd35ee0c9f4743f4a7fa568a17
Jack Reichenbach <jreichen@gustavus.edu>
2020-03-11 13:31:50 -0500

    Update transforms.js
    Done transforms

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -1,15 +1,12 @@
 // set values of mat4x4 to the identity matrix
 function Mat4x4Identity(mat4x4) {
-	mat4x4.values = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+    mat4x4.values = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
 }
 
 // set values of mat4x4 to the translate matrix
 function Mat4x4Translate(mat4x4, tx, ty, tz) {
     //mat4x4.values = ....
-    mat4x4.values = [1, 0, 0, tx, 
-    				 0, 1, 0, ty, 
-    				 0, 0, 1, tz, 
-    				 0, 0, 0, 1];
+    mat4x4.values = [[1, 0, 0, tx], [0, 1, 0, ty], [0, 0, 1, tz], [0, 0, 0, 1]];
 }
 // set values of mat4x4 to the scale matrix
 function Mat4x4Scale(mat4x4, sx, sy, sz) {
